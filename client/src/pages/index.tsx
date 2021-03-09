2f82a5eb163b59dfece422f56d1463c9507a26c0
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-10 00:07:55 +0200

    add header

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-// import logo from '../assets/logo.png';
-import logo from '../assets/images/logo.png'
-import '../index.scss'
+import Header from '@/components/header/'
+import logo from '@/assets/images/logo.png'
+import '@/index.scss'
 
 export default function src(): JSX.Element {
 	return (
-		<div className='container'>
-			<h1>Hello World!</h1>
-			<img src={logo} alt='' />
-		</div>
+		<>
+			<Header />
+			<div className='container'>
+				<h1>Hello World!</h1>
+				<img src={logo} alt='' />
+			</div>
+		</>
 	)
 }
