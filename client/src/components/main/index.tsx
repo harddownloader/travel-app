ec4e761363a53cb1c947a50808436aabf1d3c20f
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-12 19:54:16 +0200

    feat: add axion func

--- client/src/components/main/index.tsx
+++ client/src/components/main/index.tsx
@@ -1,35 +1,23 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation, Pagination, EffectFade } from 'swiper'
-import Container from '@material-ui/core/Container'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import 'swiper/swiper.scss'
 import 'swiper/components/navigation/navigation.scss'
 import 'swiper/components/pagination/pagination.scss'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import Card from './Card'
 import './swiper.css'
+import useDataApi from '@/utils/useDataApi'
+
 SwiperCore.use([Navigation, Pagination, EffectFade])
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			zIndex: 0,
-			height: '80%',
-			overflow: 'auto',
-			width: '80%',
-		},
-		swiper: {
-			height: '100%',
-			width: '100%',
-		},
-	}),
-)
-
 const Main = (): JSX.Element => {
-	const classes = useStyles()
-	//	const slideTo = index => swiper.slideTo(index)
-
+	const [
+		{ data, isLoading, isError },
+		/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+		doFetch,
+	] = useDataApi('https://travel-app-demo.herokuapp.com/countries?lang=ru', {
+		hits: [],
+	})
 	return (
 		<Swiper
 			pagination={{
@@ -50,12 +38,12 @@ const Main = (): JSX.Element => {
 					slidesPerColumn: 3,
 				},
 				1280: {
-					slidesPerView: 2,
+					slidesPerView: 3,
 					spaceBetween: 40,
 					slidesPerColumn: 3,
 				},
 			}}
-			spaceBetween={40}
+			spaceBetween={20}
 			navigation={{
 				prevEl: '.prev',
 				nextEl: '.next',
@@ -63,32 +51,38 @@ const Main = (): JSX.Element => {
 			// onSlideChange={() => console.log('slide change')}
 			// onSwiper={(swiper) => console.log(swiper)}
 		>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
-			<SwiperSlide>
-				<Card />
-			</SwiperSlide>
+			{isError && <div>Something went wrong ...</div>}
+			{isLoading ? (
+				<div>Loading ...</div>
+			) : (
+				<>
+					{!data.hits &&
+						data.map((item: Country) => (
+							<SwiperSlide
+								key={item.id}
+								onClick={() => {
+									console.log(data)
+								}}>
+								<Card />
+							</SwiperSlide>
+						))}
+				</>
+			)}
 		</Swiper>
 	)
 }
-
+type Country = {
+	ISOCode: string
+	capital: string
+	capitalLocation: {
+		coordinates: [number, number]
+		type: string
+	}
+	currency: string
+	description: string
+	id: string
+	imageUrl: string
+	name: string
+	videoUrl: string
+}
 export default Main
