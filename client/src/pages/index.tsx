77472dfe547929bbe80204dbb7f101cfddbe2ff5
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-13 04:31:27 +0200

    add context

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '@/components/header/'
 import Main from '@/components/main/'
 import '@/index.scss'
 import Footer from '@/components/footer/Footer'
+import { Context } from '@/utils/Context.jsx'
 
 export default function Src(): JSX.Element {
+	const [countries, setCountries] = useState({
+		hits: [],
+	})
+	const [leng, setLeng] = useState('ru')
+
 	return (
-		<>
-			<Header />
-			<Main />
-			<Footer />
-		</>
+		<Context.Provider
+			value={{
+				ContextLeng: [leng, setLeng],
+				ContextCountries: [countries, setCountries],
+			}}>
+			<>
+				<Header />
+				<Main />
+				<Footer />
+			</>
+		</Context.Provider>
 	)
 }
