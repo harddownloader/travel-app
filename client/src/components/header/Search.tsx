f6f3bfb376122d258566b2b46b56f139335ba5ad
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-13 04:35:57 +0200

    feat: add func search

--- client/src/components/header/Search.tsx
+++ client/src/components/header/Search.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search'
 import InputBase from '@material-ui/core/InputBase'
-
+import { Context } from '@/utils/Context.jsx'
+import axios from 'axios'
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		search: {
@@ -49,13 +50,53 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Search = (): JSX.Element => {
 	const classes = useStyles()
-
+	const { ContextCountries, ContextLeng } = useContext(Context)
+	const [countries, setCountries] = ContextCountries
+	const [value, setValue] = useState('')
+	const [leng, setLeng] = ContextLeng
+	const handlerChange = (e: any) => {
+		setValue(e.target.value)
+	}
+	useEffect(() => {
+		if (!/[а-я]/i.test(value) && leng === 'ru') return
+		if (/[а-я]/i.test(value) && leng != 'ru') return
+		const url = `https://rsschool-travel-app-be.herokuapp.com/countries?lang=${leng}`
+		let didCancel = false
+		const fetchData = async () => {
+			try {
+				const result = await axios(url)
+				if (!didCancel) {
+					setCountries(
+						result.data.filter(
+							(e: any) =>
+								e.capital
+									.toLocaleLowerCase()
+									.includes(value.toLocaleLowerCase().trim()) ||
+								e.name
+									.toLocaleLowerCase()
+									.includes(value.toLocaleLowerCase().trim()),
+						),
+					)
+				}
+			} catch (error) {
+				if (!didCancel) {
+					console.log(error)
+				}
+			}
+		}
+		fetchData()
+		return () => {
+			didCancel = true
+		}
+	}, [value, setValue])
 	return (
 		<div className={classes.search}>
 			<div className={classes.searchIcon}>
 				<SearchIcon />
 			</div>
 			<InputBase
+				value={value}
+				onChange={handlerChange.bind(this)}
 				placeholder=''
 				classes={{
 					root: classes.inputRoot,
