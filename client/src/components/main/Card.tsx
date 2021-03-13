7e95a28ed023f14fe515e0abf970303bc09cd806
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-13 04:33:27 +0200

    feat: add padding and css effect

--- client/src/components/main/Card.tsx
+++ client/src/components/main/Card.tsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import clsx from 'clsx'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Collapse from '@material-ui/core/Collapse'
 import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import { red } from '@material-ui/core/colors'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ShareIcon from '@material-ui/icons/Share'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-
+import './card.css'
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		root: {
+			cursor: 'pointer',
 			maxWidth: 345,
 			height: '90%',
+			color: '#000',
+			background: '#ccccf1',
+			transition: 'all .5s',
+			'& .MuiCard-root': {
+				paddingBottom: 5,
+			},
+			'&:hover': {
+				boxShadow: '0px 5px 10px 2px rgba(71, 73, 75, 0.2)',
+				color: '#1c2402',
+				background: 'white',
+				'& .MuiCardMedia-root': {
+					transform: 'scale(1.1)',
+				},
+			},
 		},
 		media: {
 			height: 0,
 			paddingTop: '56.25%', // 16:9
+			transition: 'all .5s',
+			'&:hover': {},
 		},
 		expand: {
 			transform: 'rotate(0deg)',
@@ -42,13 +51,18 @@ const useStyles = makeStyles((theme: Theme) =>
 	}),
 )
 
-export default function RecipeReviewCard(): JSX.Element {
+export default function RecipeReviewCard({
+	name,
+	imageimageUrl,
+	capital,
+	description,
+}: {
+	name: string
+	imageimageUrl: string
+	capital: string
+	description: string
+}): JSX.Element {
 	const classes = useStyles()
-	const [expanded, setExpanded] = React.useState(false)
-
-	const handleExpandClick = () => {
-		setExpanded(!expanded)
-	}
 
 	return (
 		<Card className={classes.root}>
@@ -58,74 +72,15 @@ export default function RecipeReviewCard(): JSX.Element {
 						R
 					</Avatar>
 				}
-				action={
-					<IconButton aria-label='settings'>
-						<MoreVertIcon />
-					</IconButton>
-				}
-				title='Shrimp and Chorizo Paella'
-				subheader='September 14, 2016'
+				title={name}
+				subheader={capital}
 			/>
-			<CardMedia
-				className={classes.media}
-				image='/static/images/cards/paella.jpg'
-				title='Paella dish'
-			/>
+			<CardMedia className={classes.media} image={imageimageUrl} title={name} />
 			<CardContent>
-				<Typography variant='body2' color='textSecondary' component='p'>
-					This impressive paella is a perfect party dish and a fun meal to cook
-					together with your guests. Add 1 cup of frozen peas along with the
-					mussels, if you like.
+				<Typography variant='body2' className='card-info' component='p'>
+					{description}
 				</Typography>
 			</CardContent>
-			<CardActions disableSpacing>
-				<IconButton aria-label='add to favorites'>
-					<FavoriteIcon />
-				</IconButton>
-				<IconButton aria-label='share'>
-					<ShareIcon />
-				</IconButton>
-				<IconButton
-					className={clsx(classes.expand, {
-						[classes.expandOpen]: expanded,
-					})}
-					onClick={handleExpandClick}
-					aria-expanded={expanded}
-					aria-label='show more'>
-					<ExpandMoreIcon />
-				</IconButton>
-			</CardActions>
-			<Collapse in={expanded} timeout='auto' unmountOnExit>
-				<CardContent>
-					<Typography paragraph>Method:</Typography>
-					<Typography paragraph>
-						Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-						set aside for 10 minutes.
-					</Typography>
-					<Typography paragraph>
-						Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-						over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-						stirring occasionally until lightly browned, 6 to 8 minutes.
-						Transfer shrimp to a large plate and set aside, leaving chicken and
-						chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-						onion, salt and pepper, and cook, stirring often until thickened and
-						fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-						cups chicken broth; bring to a boil.
-					</Typography>
-					<Typography paragraph>
-						Add rice and stir very gently to distribute. Top with artichokes and
-						peppers, and cook without stirring, until most of the liquid is
-						absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-						shrimp and mussels, tucking them down into the rice, and cook again
-						without stirring, until mussels have opened and rice is just tender,
-						5 to 7 minutes more. (Discard any mussels that don’t open.)
-					</Typography>
-					<Typography>
-						Set aside off of the heat to let rest for 10 minutes, and then
-						serve.
-					</Typography>
-				</CardContent>
-			</Collapse>
 		</Card>
 	)
 }
